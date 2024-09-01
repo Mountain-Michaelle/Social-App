9b3e1cb24b8cfaeac57d1a90bb8196f9caa67715
unknown <Mountain-Michaelle>
2024-09-01 16:56:48 +0100

    adding bookmark with javascript

---
+++ templates/bookmarklet_launcher.js
@@ -0,0 +1,12 @@
+(function(){
+    if(!window.bookmarklet) {
+    bookmarklet_js = document.body.appendChild(document.
+    createElement('script'));
+    bookmarklet_js.src = '//mysite.com:8000/static/Assets/js/bookmarklet.js?r='+Math.
+    floor(Math.random()*9999999999999999);
+    window.bookmarklet = true;
+    }
+    else {
+    bookmarkletLaunch();
+    }
+    })();
