573e2bdbc06eecfd87989129e6d2fa72ecc09bf4
unknown <Mountain-Michaelle>
2024-09-02 17:44:21 +0100

    still trying to deploy

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ python3.9 -m pip install -r requirements.txt
 echo "Make migrations..."
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
-python manage.py compress
+python3.9 manage.py compress
 python3.9 manage.py collectstatic --noinput
 
 
