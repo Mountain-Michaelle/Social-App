33607ae243a1a6b6e0a4938a443afd0780da68fd
unknown <Mountain-Michaelle>
2024-09-02 08:08:49 +0100

    updated my static settings

--- social_app/settings.py
+++ social_app/settings.py
@@ -188,7 +188,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR /'staticfiles/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
