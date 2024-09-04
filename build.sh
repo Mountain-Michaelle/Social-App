6b53c680901d245e6d11f106c22486328edee1ed
unknown <Mountain-Michaelle>
2024-09-04 18:58:37 +0100

    updated json files

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 # Build the project
 
 echo 'Building the project...'
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 
 
 echo "Make migrations..."
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py compress --noinput
