2583f700dc84e27faeeca6406b5ff569d5b935f9
unknown <Mountain-Michaelle>
2024-09-04 18:54:34 +0100

    updated json files

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ python3.9 -m pip install -r requirements.txt
 echo "Make migrations..."
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
-python3.9 manage.py collectstatic --force
-python3.9 manage.py compress --force
+python manage.py collectstatic --noinput
+python manage.py compress --noinput
