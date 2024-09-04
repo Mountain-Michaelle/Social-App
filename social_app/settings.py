6781a8a9527610c77fa0b4d4962bf10861b76346
unknown <Mountain-Michaelle>
2024-09-04 12:32:16 +0100

    retesting 02

--- social_app/settings.py
+++ social_app/settings.py
@@ -189,7 +189,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[BASE_DIR/'static/']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
