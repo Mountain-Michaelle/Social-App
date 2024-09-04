633db1fd39dfb7ca96a0dcd076d5b6947c3b8b81
unknown <Mountain-Michaelle>
2024-09-04 16:19:29 +0100

    installed pip

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 
 # Build the project
+if ! command -v pip &> /dev/null
+then
+    echo "pip could not be found. Installing pip..."
+    apt-get update && apt-get install -y python3-pip
+fi
 
 echo 'Building the project...'
 python3.9 -m pip install -r requirements.txt
