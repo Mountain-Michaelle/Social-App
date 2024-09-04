5d69f0aca8f02ef2a41701bc73d8339418592926
unknown <Mountain-Michaelle>
2024-09-04 19:03:15 +0100

    modified my build

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 
 # Build the project
-
 echo 'Building the project...'
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 
 
 echo "Make migrations..."
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py compress --noinput
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput
+python3.9 manage.py compress --noinput
